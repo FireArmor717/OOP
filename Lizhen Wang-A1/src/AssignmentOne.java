ea6f1bfc306354ea736f821569421289cc4c3c0b
FireArmor717 <1621870756@qq.com>
2024-11-15 16:54:44 +0800

    finish part of A1 class

--- Lizhen Wang-A1/src/AssignmentOne.java
+++ Lizhen Wang-A1/src/AssignmentOne.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class AssignmentOne {
     public static void main(String[] args) {
-        GeneralPractitioner gp1=new GeneralPractitioner(1,"Zhen","GP Department","Check");
-        GeneralPractitioner gp2=new GeneralPractitioner(2,"Xiao","GP Department",);
+        GeneralPractitioner gp1=new GeneralPractitioner(1,"Jun","GP Department","Check");
+        GeneralPractitioner gp2=new GeneralPractitioner(2,"Xiao","GP Department","Obeservation");
+        TCM c1=new TCM(3,"Zhen","TCM Department","Medicine and Surgery");
+
+        ArrayList<Appointment>appts=new ArrayList<>();
+        appts.add(new Appointment("18539251669","Xiang","09:33",gp1));
+        appts.add(new Appointment("17722223838","Atlas","18:18",gp2));
+        appts.add(new Appointment("13322221111","Lava","10:10",c1));
+
+        System.out.println("Current Appointments: ");
+        for()
     }
 }
