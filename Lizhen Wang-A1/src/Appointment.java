819a608b53a776eb21680ba646e5d38476ff3a82
FireArmor717 <1621870756@qq.com>
2024-11-15 16:12:35 +0800

    Removes unnecesary empty constructor statements and finish the appointment class

--- Lizhen Wang-A1/src/Appointment.java
+++ Lizhen Wang-A1/src/Appointment.java
@@ -1,3 +1,28 @@
 public class Appointment {
-    
+    public static Object phone;
+    private String PatientName;
+    private String PatientMobileNum;
+    private HealthProfessional healthProfessional;
+    private String TimeSlot;
+
+    public Appointment() {
+    }
+
+    public Appointment(String PatientMobileNum, String PatientName, String TimeSlot, HealthProfessional healthProfessional) {
+        this.PatientMobileNum = PatientMobileNum;
+        this.PatientName = PatientName;
+        this.TimeSlot = TimeSlot;
+        this.healthProfessional = healthProfessional;
+    }
+
+    public void PrintDetails(){
+        System.out.println("Patient Name: "+PatientName);
+        System.out.println("Patient Moblie Num: "+PatientMobileNum);
+        if(healthProfessional!=null){
+            System.out.println("Health Professional Details: "+healthProfessional);
+        }else{
+            System.out.println("No Health Professional were assigned.");
+        }    
+        System.out.println("Time slot"+TimeSlot);
+    }
 }
