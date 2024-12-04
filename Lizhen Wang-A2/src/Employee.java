ae9790b962be4d4802053458835e8e331a95f677
FireArmor717 <1621870756@qq.com>
2024-12-04 09:41:13 +0800

    2.	I have the Employee and Visitor classes inherit the Person class, with the Employee class representing the employee (such as an operator) and the Visitor class, respectively Classes represent tourists (e.g., visitors to play).

--- Lizhen Wang-A2/src/Employee.java
+++ Lizhen Wang-A2/src/Employee.java
@@ -1,3 +1,18 @@
-public class Employee {
+public class Employee extends Person {
+    private String role;
+    private int employeeId;
 
+    public Employee() { super(); }
+
+    public Employee(String name, int age, String gender, String role, int employeeId) {
+        super(name, age, gender);
+        this.role = role;
+        this.employeeId = employeeId;
+    }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public int getEmployeeId() { return employeeId; }
+    public void setEmployeeId(int employeeId) { this.employeeId = employeeId; }
 }
--- Lizhen Wang-A2/src/Visitor.java
+++ Lizhen Wang-A2/src/Visitor.java
@@ -1,3 +1,18 @@
-public class Visitor {
+public class Visitor extends Person {
+    private int visitorId;
+    private boolean isVIP;
 
+    public Visitor() { super(); }
+
+    public Visitor(String name, int age, String gender, int visitorId, boolean isVIP) {
+        super(name, age, gender);
+        this.visitorId = visitorId;
+        this.isVIP = isVIP;
+    }
+
+    public int getVisitorId() { return visitorId; }
+    public void setVisitorId(int visitorId) { this.visitorId = visitorId; }
+
+    public boolean isVIP() { return isVIP; }
+    public void setVIP(boolean isVIP) { this.isVIP = isVIP; }
 }
