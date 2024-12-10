2cadc864d56eb4e578fdfcb40693ea4abe8cf570
FireArmor717 <1621870756@qq.com>
2024-12-10 17:13:25 +0800

    1.	Sorting implementation: I've created a VisitorComparator class that implements the Comparator interface. The compare method sorts visitors based on their names. I then use the Collections.sort() method in the Ride class to sort the history of visitors.

--- Lizhen Wang-A2/src/VisitorComparator.java
+++ Lizhen Wang-A2/src/VisitorComparator.java
@@ -1,3 +1,8 @@
-public class VisitorComparator {
+import java.util.Comparator;
 
+public class VisitorComparator implements Comparator<Visitor> {
+    @Override
+    public int compare(Visitor v1, Visitor v2) {
+        return v1.getName().compareTo(v2.getName());
+    }
 }
