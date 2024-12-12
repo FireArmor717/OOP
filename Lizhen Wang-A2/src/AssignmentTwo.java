34212c9ad541a3929390c3ec2361d5371d311858
FireArmor717 <1621870756@qq.com>
2024-12-12 17:15:51 +0800

    Add assignment objects

--- Lizhen Wang-A2/src/AssignmentTwo.java
+++ Lizhen Wang-A2/src/AssignmentTwo.java
@@ -1,3 +1,11 @@
 public class AssignmentTwo {
-
-}
+    public static void main(String[] args) {
+        AssignmentTwo assignment = new AssignmentTwo();
+        assignment.partThree();
+        assignment.partFourA();
+        assignment.partFourB();
+        assignment.partFive();
+        assignment.partSix();
+        assignment.partSeven();
+    }
+    
