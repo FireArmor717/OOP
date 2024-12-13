572bf63e79732fd9759d52b83c0aad833b3aa726
FireArmor717 <1621870756@qq.com>
2024-12-13 14:22:23 +0800

    Create objects of visitors as the task description and finish the part 3

--- Lizhen Wang-A2/src/AssignmentTwo.java
+++ Lizhen Wang-A2/src/AssignmentTwo.java
@@ -8,4 +8,27 @@ public class AssignmentTwo {
         assignment.partSix();
         assignment.partSeven();
     }
-    
+
+    // Part 3: Queue operations
+    public void partThree() {
+        System.out.println("Part 3: Queue operations output");
+
+        Employee operator = new Employee("Junyi", 30, "Male", "Operator", 1);
+        Ride rollerCoaster = new Ride("Roller Coaster", operator);
+
+        Visitor visitor1 = new Visitor("Xiao", 25, "Female", 101, true);
+        Visitor visitor2 = new Visitor("Zhen", 20, "Male", 102, false);
+        Visitor visitor3 = new Visitor("Charlie", 35, "Male", 103, false);
+        Visitor visitor4 = new Visitor("David", 28, "Male", 104, true);
+        Visitor visitor5 = new Visitor("Eve", 22, "Female", 105, false);
+
+        rollerCoaster.addVisitorToQueue(visitor1);
+        rollerCoaster.addVisitorToQueue(visitor2);
+        rollerCoaster.addVisitorToQueue(visitor3);
+        rollerCoaster.addVisitorToQueue(visitor4);
+        rollerCoaster.addVisitorToQueue(visitor5);
+
+        rollerCoaster.printQueue();
+        rollerCoaster.removeVisitorFromQueue();
+        rollerCoaster.printQueue();
+    }
