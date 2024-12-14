8dc88a3dfe3260ffcb136735270964a8d1739fb0
FireArmor717 <1621870756@qq.com>
2024-12-14 12:20:02 +0800

    Finish part4a

--- Lizhen Wang-A2/src/AssignmentTwo.java
+++ Lizhen Wang-A2/src/AssignmentTwo.java
@@ -32,3 +32,29 @@ public class AssignmentTwo {
         rollerCoaster.removeVisitorFromQueue();
         rollerCoaster.printQueue();
     }
+    // Part 4A: Use LinkedList to store Ride history
+    public void partFourA() {
+        System.out.println("Part 4A: Use LinkedList to store Ride history output");
+
+        Employee operator = new Employee("Junyi", 30, "Male", "Operator", 1);
+        Ride rollerCoaster = new Ride("Roller Coaster", operator);
+
+        Visitor visitor1 = new Visitor("Xiao", 25, "Female", 101, true);
+        Visitor visitor2 = new Visitor("Zhen", 20, "Male", 102, false);
+        Visitor visitor3 = new Visitor("Charlie", 35, "Male", 103, false);
+        Visitor visitor4 = new Visitor("David", 28, "Male", 104, true);
+        Visitor visitor5 = new Visitor("Eve", 22, "Female", 105, false);
+
+        rollerCoaster.addVisitorToQueue(visitor1);
+        rollerCoaster.addVisitorToQueue(visitor2);
+        rollerCoaster.addVisitorToQueue(visitor3);
+        rollerCoaster.addVisitorToQueue(visitor4);
+        rollerCoaster.addVisitorToQueue(visitor5);
+        rollerCoaster.addVisitorToHistory(visitor1);
+        rollerCoaster.addVisitorToHistory(visitor2);
+        rollerCoaster.addVisitorToHistory(visitor3);
+
+        rollerCoaster.printRideHistory();
+        System.out.println("Visitor in history: " + rollerCoaster.checkVisitorFromHistory(visitor1));
+    }
+}
